b3a0181e9996da3307f10dbacd2c1aa76ae90c73
Alaa Eddin Albarghoth <alaaeddin.albarghoth@appcent.mobi>
2022-06-22 20:40:49 +0300

    Clean the code

--- app/src/main/java/com/alaaeddinalbarghoth/testcicdapp/ui/theme/Color.kt
+++ app/src/main/java/com/alaaeddinalbarghoth/testcicdapp/ui/theme/Color.kt
@@ -1,8 +1,11 @@
 package com.alaaeddinalbarghoth.testcicdapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
-
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+const val PURPLE_200_CONS = 0xFFBB86FC
+const val PURPLE_500_CONS = 0xFF6200EE
+const val PURPLE_700_CONS = 0xFF3700B3
+const val TEAL_200_CONS = 0xFF03DAC5
+val Purple200 = Color(PURPLE_200_CONS)
+val Purple500 = Color(PURPLE_500_CONS)
+val Purple700 = Color(PURPLE_700_CONS)
+val Teal200 = Color(TEAL_200_CONS)
